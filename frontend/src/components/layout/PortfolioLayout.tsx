bbb10145683494695c41f74d4737b7ed3a82efd6
Edem Ahorlu <edemcapp101@gmail.com>
2026-03-20 14:45:34 -0400

    feat: make only left pane scrollable. install react icons

--- frontend/src/components/layout/PortfolioLayout.tsx
+++ frontend/src/components/layout/PortfolioLayout.tsx
@@ -8,14 +8,14 @@ export default function CommandCenterLayout({
   rightPane,
 }: PortfolioLayoutProps) {
   return (
-    <main className="flex min-h-screen w-full text-zinc-50">
-      <section className="w-1/2 min-h-screen overflow-y-auto border-r border-zinc-800">
+    <main className="flex h-screen w-full overflow-hidden text-zinc-50">
+      <section className="h-screen w-1/2 overflow-y-auto border-r border-zinc-800">
         <div className="mx-auto flex w-full max-w-4xl flex-col gap-20 px-10 py-16 lg:px-16">
           {leftPane}
         </div>
       </section>
 
-      <aside className="w-1/2 min-h-screen">
+      <aside className="h-screen w-1/2 overflow-hidden">
         {rightPane}
       </aside>
     </main>
