2b9edc822513e90b5952af827fcf936baa686eb8
Edem Ahorlu <edemcapp101@gmail.com>
2026-03-21 06:16:41 -0400

    feat: add title to capabilites component. import material symbols

--- frontend/src/components/portfolio/Capabilities.tsx
+++ frontend/src/components/portfolio/Capabilities.tsx
@@ -1,5 +1,18 @@
 export default function Capabilities() {
     return (
-        <>This is Capabilities</>
+        <section className="flex flex-col gap-8 mt-8 border-t border-border-color pt-12">
+
+            <div className="flex items-center gap-3 mb-2">
+                <span className="material-symbols-outlined text-primary">memory</span>
+                <h3 className="text-xl font-mono font-bold">Capabilities</h3>
+            </div>
+
+
+
+
+
+            
+            
+        </section>
     )
 }
