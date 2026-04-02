e79f1977b0c25063deaf20099aa396ba1500144b
Edem Ahorlu <edemcapp101@gmail.com>
2026-04-02 15:48:17 -0400

    chore: add profile resource

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), ChatModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), ChatModule, ProfileModule],
   controllers: [AppController],
   providers: [AppService],
 })
